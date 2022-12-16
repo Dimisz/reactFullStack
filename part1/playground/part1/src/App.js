cbe2ea11fba1cea2732cb14a2c638c311de7ee40
Vladimir Solovyov <leilyngale@icloud.com>
2022-12-16 21:43:58 +0800

    Basic event handlers/ added simple counter

--- part1/playground/part1/src/App.js
+++ part1/playground/part1/src/App.js
@@ -1,10 +1,29 @@
 import Hello from "./components/Hello";
 import Footer from "./components/Footer";
 
+import { useState } from "react";
+
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increaseByOne = () => {
+    setCount(count + 1);
+  };
+
+  const decreaseByOne = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
   return(
     <>
-      <h1>Greetings</h1>
+      <h1>Count: {count} </h1>
+      <button onClick={increaseByOne}> plus </button>
+      <button onClick={decreaseByOne}> minus </button>
+      <button onClick={reset}> Reset </button>
       <Hello name="Michael" age={20} />
       <Hello name="Peter" age={18} />
       <Hello name="Sarah" age={21} />
--- part1/playground/part1/src/components/Hello.js
+++ part1/playground/part1/src/components/Hello.js
@@ -1,7 +1,12 @@
 const Hello = ({ name, age }) => {
+  const yearBorn = () => {
+    const yearNow = new Date().getFullYear();
+    return yearNow - age;
+  }
   return(
     <div>
       <p>Hello {name}! You are {age} years old.</p>
+      <p>So you were probably born in {yearBorn()}.</p>
     </div>
   );
 }
