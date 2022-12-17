4c2a54be68b6df1b9843e85d8444c9405710222b
Vladimir Solovyov <leilyngale@icloud.com>
2022-12-17 14:56:34 +0800

    2.2 - 2.3*: course information steps 2 - 3

--- part2/exercises/course-information/src/components/Course.js
+++ part2/exercises/course-information/src/components/Course.js
@@ -2,10 +2,13 @@ import Header from "./Header";
 import Content from "./Content";
 
 const Course = ({ course }) => {
+  const total = course.parts.reduce((accum, part) => accum + part.exercises, 0);
+
   return(
     <>
       <Header title={course.name} />
       <Content parts={course.parts} />
+      <h3>total of {total} exercises</h3>
     </>
   );
 }
